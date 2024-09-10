6ae02930906ba82b1b36168fb11f21ee8f5763f2
hugo assal <hugo.assal1@etu.univ-lorraine.fr>
2024-09-10 17:32:44 +0200

    ajout rdv

--- app/src/core/domain/entities/rendezvous/RendezVous.php
+++ app/src/core/domain/entities/rendezvous/RendezVous.php
@@ -2,7 +2,20 @@
 
 namespace toubeelib\core\domain\entities\rendezvous;
 
-class RendezVous
+use toubeelib\core\domain\entities\Entity;
+use toubeelib\core\domain\entities\praticien\Praticien;
+use toubeelib\core\domain\entities\praticien\Specialite;
+
+class RendezVous extends Entity
 {
+
+    protected string $idPatient;
+
+    protected \DateTime $creneau;
+
+    protected Praticien $praticien;
+
+    protected Specialite $specialitee;
+
 
 }
