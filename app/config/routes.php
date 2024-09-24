7a2fb3eeb6580c926a4a04af082f2ac38ac7563a
hugo assal <hugo.assal1@etu.univ-lorraine.fr>
2024-09-24 14:54:53 +0200

    Demarrage de l'API

--- app/config/routes.php
+++ app/config/routes.php
@@ -3,11 +3,14 @@ declare(strict_types=1);
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use toubeelib\application\actions\ConsulterRendezVousAction;
+use toubeelib\application\actions\HomeAction;
 
 return function( \Slim\App $app):\Slim\App {
 
-    $app->get('/', \toubeelib\application\actions\HomeAction::class);
+    $app->get('/', HomeAction::class);
 
+    $app->get('/rdvs/{ID-RDV}', ConsulterRendezVousAction::class);
 
     return $app;
 };
---
+++ app/src/application/actions/ConsulterRendezVousAction.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace toubeelib\application\actions;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use toubeelib\application\renderer\JsonRenderer;
+use toubeelib\core\services\rdv\ServiceRendezVous;
+use toubeelib\core\services\rdv\ServiceRendezVousInvalidDataException;
+use toubeelib\infrastructure\repositories\ArrayRdvRepository;
+
+class ConsulterRendezVousAction
+{
+
+    public function __invoke(ServerRequestInterface $rq, ResponseInterface $rs, array $args)
+    {
+
+        //On essaye de récupérer l'id donné en paramètre
+        $id = $args['id'] ?? null;
+
+        $service = new ServiceRendezVous(new ArrayRdvRepository());
+
+        try {
+            $rdv = $service->getRendezVousById($id);
+            return JsonRenderer::render($rs, 200, $rdv);
+        } catch (ServiceRendezVousInvalidDataException $e) {
+            return JsonRenderer::render($rs, 400, ['error' => $e->getMessage()]);
+        }
+
+    }
+}
