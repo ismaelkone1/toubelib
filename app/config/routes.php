244f04cdca6aeb29094623e5a098e284a06f5307
hugo assal <hugo.assal1@etu.univ-lorraine.fr>
2024-10-21 17:35:36 +0200

    Ajout des headers et modif route creer rendez vous

--- app/config/routes.php
+++ app/config/routes.php
@@ -10,41 +10,47 @@ use toubeelib\application\actions\ListerRendezVousPatientAction;
 use toubeelib\application\middlewares\AddHeaders;
 use toubeelib\application\actions\AnnulerRendezVousAction;
 use toubeelib\application\actions\ConsulterRendezVousAction;
-use toubeelib\application\actions\CreerRendezVous;
 use toubeelib\application\actions\CreerRendezVousAction;
 use toubeelib\application\actions\HomeAction;
-use toubeelib\application\actions\ModifierRendezVousAction;
+use toubeelib\application\actions\ModifierOuGererCycleRendezVousAction;
 
 return function( \Slim\App $app):\Slim\App {
 
     $app->get('/', HomeAction::class)
     ->add(new AddHeaders);
 
-    $app->post('/prendre-rdvs/', CreerRendezVousAction::class)
+    $app->post('/rdvs', CreerRendezVousAction::class)
     ->add(new AddHeaders);
 
     $app->get('/rdvs/{ID-RDV}', ConsulterRendezVousAction::class)
     ->add(new AddHeaders);
 
-    $app->patch('/rdvs/{ID-RDV}', ModifierRendezVousAction::class)
+    $app->patch('/rdvs/{ID-RDV}', ModifierOuGererCycleRendezVousAction::class)
     ->add(new AddHeaders);
 
     $app->delete('/rdvs/{ID-RDV}', AnnulerRendezVousAction::class)
     ->add(new AddHeaders);
 
     //La route s'utilise de la manière suivante : /praticiens/{ID-PRATICIEN}/disponibilites?debut=2021-06-01T08:00:00&fin=2021-06-01T18:00:00
-    $app->get('/praticiens/{ID-PRATICIEN}/disponibilites', ListerDispoPraticienAction::class);
+    $app->get('/praticiens/{ID-PRATICIEN}/disponibilites', ListerDispoPraticienAction::class)
+    ->add(new AddHeaders);
 
     //La route s'utilise de la manière suivante : /rdvs/{ID-RDV}/cycle?statut=2 pour mettre le rdv en "honore"
-    $app->patch('/rdvs/{ID-RDV}/cycle', GererCycleRendezVousAction::class);
+    $app->patch('/rdvs/{ID-RDV}/cycle', GererCycleRendezVousAction::class)
+    ->add(new AddHeaders);
 
-    $app->get('/praticiens/{ID-PRATICIEN}', ConsulterPraticienAction::class);
+    $app->get('/praticiens/{ID-PRATICIEN}', ConsulterPraticienAction::class)
+    ->add(new AddHeaders);
 
-    $app->get('/praticiens', ConsulterListePraticiensAction::class);
+    $app->get('/praticiens', ConsulterListePraticiensAction::class)
+    ->add(new AddHeaders);
 
     //La route s'utilise de la manière suivante : /praticiens/4g5h6i7j-8901-1121-3141-6171k9l0m1n2/planning?debut=2024-06-01T08:00:00&fin=2025-06-01T18:00:00&specialitee=CAR&type=consultation
-    $app->get('/praticiens/{ID-PRATICIEN}/planning', ConsulterPlanningPraticienAction::class);
+    $app->get('/praticiens/{ID-PRATICIEN}/planning', ConsulterPlanningPraticienAction::class)
+    ->add(new AddHeaders);
 
-    $app->get('/patients/{ID-PATIENT}/rdvs', ListerRendezVousPatientAction::class);
+    $app->get('/patients/{ID-PATIENT}/rdvs', ListerRendezVousPatientAction::class)
+    ->add(new AddHeaders);
+
     return $app;
 };
---
+++ app/src/application/actions/ModifierOuGererCycleRendezVousAction.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace toubeelib\application\actions;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Respect\Validation\Exceptions\NestedValidationException;
+use Slim\Exception\HttpBadRequestException;
+use toubeelib\application\renderer\JsonRenderer;
+use toubeelib\core\dto\GererCycleRendezVousDTO;
+use toubeelib\core\dto\ModificationRendezVousDTO;
+use toubeelib\core\services\rdv\ServiceRendezVousInterface;
+use toubeelib\core\services\rdv\ServiceRendezVousInvalidDataException;
+use Respect\Validation\Validator as v;
+
+class ModifierOuGererCycleRendezVousAction extends AbstractAction
+{
+    private ServiceRendezVousInterface $serviceRendezVousInterface;
+
+    public function __construct(ServiceRendezVousInterface $serviceRendezVousInterface)
+    {
+        $this->serviceRendezVousInterface = $serviceRendezVousInterface;
+    }
+
+    public function __invoke(ServerRequestInterface $rq, ResponseInterface $rs, array $args): ResponseInterface
+    {
+        $data = $rq->getParsedBody();
+
+        if (isset($data['statut'])) {
+            return $this->handleCycleRdv($rq, $rs, $args, $data);
+        }
+
+        return $this->handleModificationRdv($rq, $rs, $args, $data);
+    }
+
+    private function handleCycleRdv(ServerRequestInterface $rq, ResponseInterface $rs, array $args, array $data): ResponseInterface
+    {
+        $gererCyclerdvDto = new GererCycleRendezVousDTO($args['ID-RDV'], $data['statut']);
+        $this->validateDto($gererCyclerdvDto, $rq);
+
+        $this->validateInput($args['ID-RDV'], $data['statut'], $rq);
+
+        try {
+            $rdv = $this->serviceRendezVousInterface->gererCycleRdv($gererCyclerdvDto);
+            return $this->renderResponse($rs, $rdv, $args['ID-RDV']);
+        } catch (ServiceRendezVousInvalidDataException $e) {
+            throw new HttpBadRequestException($rq, $e->getMessage());
+        }
+    }
+
+    private function handleModificationRdv(ServerRequestInterface $rq, ResponseInterface $rs, array $args, array $data): ResponseInterface
+    {
+        $this->validateId($args['ID-RDV'], $rq);
+        $this->validateModificationInput($data, $rq);
+
+        $this->validateInput($args['ID-RDV'], $data['specialitee'], $rq, $data['patient']);
+
+        $modifierRdvDto = new ModificationRendezVousDTO($args['ID-RDV'], $data['patient'], $data['specialitee']);
+
+        try {
+            $rdv = $this->serviceRendezVousInterface->modifierRendezvous($modifierRdvDto);
+            return $this->renderResponse($rs, $rdv, $args['ID-RDV']);
+        } catch (ServiceRendezVousInvalidDataException $e) {
+            throw new HttpBadRequestException($rq, $e->getMessage());
+        }
+    }
+
+    private function validateDto($dto, ServerRequestInterface $rq): void
+    {
+        $validator = v::attribute('id', v::stringType()->notEmpty())
+            ->attribute('statut', v::stringType()->notEmpty());
+
+        $dto->setBusinessValidator($validator);
+
+        try {
+            $dto->validate();
+        } catch (NestedValidationException $e) {
+            throw new HttpBadRequestException($rq, $e->getMessage());
+        }
+    }
+
+    private function validateId(string $id, ServerRequestInterface $rq): void
+    {
+        $idValidator = v::stringType()->notEmpty();
+
+        try {
+            $idValidator->assert($id);
+        } catch (NestedValidationException $e) {
+            throw new HttpBadRequestException($rq, $e->getMessage());
+        }
+    }
+
+    private function validateModificationInput(array $data, ServerRequestInterface $rq): void
+    {
+        $modifierRdvInputValidator = v::key('specialitee', v::optional(v::stringType()->notEmpty()))
+            ->key('patient', v::optional(v::stringType()->notEmpty()));
+
+        try {
+            $modifierRdvInputValidator->assert($data);
+        } catch (NestedValidationException $e) {
+            throw new HttpBadRequestException($rq, $e->getMessage());
+        }
+    }
+
+    private function validateInput(string $id, string $specialitee, ServerRequestInterface $rq, string $patient = null): void
+    {
+        if ((filter_var($id, FILTER_SANITIZE_FULL_SPECIAL_CHARS)) !== $id ||
+            (filter_var($specialitee, FILTER_SANITIZE_FULL_SPECIAL_CHARS)) !== $specialitee ||
+            ($patient !== null && (filter_var($patient, FILTER_SANITIZE_FULL_SPECIAL_CHARS)) !== $patient)) {
+            throw new HttpBadRequestException($rq, "Bad data format");
+        }
+    }
+
+    private function renderResponse(ResponseInterface $rs, $rdv, string $idRdv): ResponseInterface
+    {
+        $data = [
+            'rdv' => $rdv,
+            'links' => [
+                'self' => ["href" => '/rdv/' . $idRdv],
+                'modifier' => ["href" => '/rdv/' . $idRdv],
+                'annuler' => ["href" => '/rdv/' . $idRdv],
+                'praticien' => ["href" => '/praticien/' . $rdv->getPraticien()],
+                'patient' => ["href" => '/patient/' . $rdv->getIdPatient()]
+            ]
+        ];
+
+        return JsonRenderer::render($rs, 200, $data);
+    }
+}
